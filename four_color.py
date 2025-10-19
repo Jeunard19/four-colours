4dfc09155970e8010239e972c9fb7fcee6f92c74
Jeunard Boekhoudt <j.boekhoudt@bereslim.nl>
2025-10-19 17:44:36 +0200

    feat: visual added

--- four_color.py
+++ four_color.py
@@ -1,3 +1,7 @@
+from pyvis.network import Network
+import networkx as nx
+net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
+
 adj = [
     [ 0, 1, 1, 1, 0 ],
     [ 1, 0, 1, 0, 0 ],
@@ -16,14 +20,6 @@ adj = [
     [1, 0, 0, 0, 1, 1, 0]
 ]
 
-adj = [
-    [0, 1, 0, 1, 0, 1],  # A
-    [1, 0, 1, 1, 0, 0],  # B
-    [0, 1, 0, 0, 1, 0],  # C
-    [1, 1, 0, 0, 1, 0],  # D
-    [0, 0, 1, 1, 0, 1],  # E
-    [1, 0, 0, 0, 1, 0]   # F
-]
 
 
 
@@ -33,6 +29,7 @@ colors = [1,2,3,4]
 
 
 mapps = {}
+g = Network()
 
 
 for s,i in enumerate(adj):
@@ -62,7 +59,26 @@ for key, value in mapps.items():
 print(mapps)
 print(results)
 
+vis_colors =["red","green","blue","yellow"]
+print(vis_colors[1])
 
+true_colors = [vis_colors[(x-1)] for x in list(results.values())]
+print(true_colors)
+nodes=list(results.keys())
+net.add_nodes(nodes,color=true_colors) 
+print(list(results.values()))
+for key, value in mapps.items():
+	for i in value:
+		net.add_edge(key,i)
+
+
+
+
+g = Network()
+#g.add_node(0)
+#g.add_node(1)
+#g.add_edge(0, 1)
+net.show('test.html', notebook=False)
 
 
 
