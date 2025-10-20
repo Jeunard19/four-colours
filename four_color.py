45cfa198e18374ddba96e1d1424a640e398b2a8b
Jeunard Boekhoudt <j.boekhoudt@bereslim.nl>
2025-10-20 15:47:56 +0200

    chore: unit test and readme added

--- four_color.py
+++ four_color.py
@@ -6,11 +6,7 @@ def create_bording_region_dict(adj):
 	bording_region_dict= {}
 	for s,i in enumerate(adj):
 		key = f"Region_{s}"
-		bording_region_dict[key] = []
-		for k,x in enumerate(i):
-			if x == 1:
-				region = f"Region_{k}"
-				bording_region_dict[key].append(region)
+		bording_region_dict[key] = {f"Region_{k}" for k,x in enumerate(i) if x == 1}
 	return bording_region_dict
 
 def create_initial_results_dict(bording_region_dict):
@@ -22,11 +18,11 @@ def create_initial_results_dict(bording_region_dict):
 def allocate_color(bording_region_dict, results_dict, minimize=False):
 	colors = [1,2,3,4]
 	count=0
-	regions = list(bording_region_dict.keys())
+	regions = set(bording_region_dict.keys())
 	for key, value in bording_region_dict.items():
 		# This is for minimizing color use
 		if minimize:
-			non_bording_regions = [i for i in regions if i not in value]
+			non_bording_regions = {i for i in regions if i not in value}
 			for region in non_bording_regions:
 				if results_dict[key] != results_dict[region]:
 					results_dict[key]=results_dict[region]
@@ -39,7 +35,6 @@ def allocate_color(bording_region_dict, results_dict, minimize=False):
 				except IndexError:
 					count=0
 					results_dict[key] = colors[count]
-	return results_dict
 
 
 def create_connected_node_network(bording_region_dict, results_dict):
@@ -58,6 +53,7 @@ def create_connected_node_network(bording_region_dict, results_dict):
 
 
 def main():
+	# Provide your adjacency matrix
     adj = [
         [ 0, 1, 1, 1, 0 ],
         [ 1, 0, 1, 0, 0 ],
@@ -65,50 +61,15 @@ def main():
         [ 1, 0, 1, 0, 1 ], 
         [ 0, 0, 0, 1, 0 ]  
     ]
-    adj = [
-    [0, 1, 1, 0, 0, 0, 0],  # Node 0
-    [1, 0, 1, 1, 0, 0, 0],  # Node 1
-    [1, 1, 0, 0, 1, 0, 0],  # Node 2
-    [0, 1, 0, 0, 1, 1, 0],  # Node 3
-    [0, 0, 1, 1, 0, 0, 1],  # Node 4
-    [0, 0, 0, 1, 0, 0, 1],  # Node 5
-    [0, 0, 0, 0, 1, 1, 0]   # Node 6
-]
-    adj = [
-    [0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],  # 0
-    [1,0,1,1,0,0,0,0,0,0,0,0,0,0,0],  # 1
-    [1,1,0,0,1,0,0,0,0,0,0,0,0,0,0],  # 2
-    [0,1,0,0,1,1,0,0,0,0,0,0,0,0,0],  # 3
-    [0,0,1,1,0,0,1,0,0,0,0,0,0,0,0],  # 4
-    [0,0,0,1,0,0,1,1,0,0,0,0,0,0,0],  # 5
-    [0,0,0,0,1,1,0,1,1,0,0,0,0,0,0],  # 6
-    [0,0,0,0,0,1,1,0,1,1,0,0,0,0,0],  # 7
-    [0,0,0,0,0,0,1,1,0,1,1,0,0,0,0],  # 8
-    [0,0,0,0,0,0,0,1,1,0,1,1,0,0,0],  # 9
-    [0,0,0,0,0,0,0,0,1,1,0,1,1,0,0],  # 10
-    [0,0,0,0,0,0,0,0,0,1,1,0,1,1,0],  # 11
-    [0,0,0,0,0,0,0,0,0,0,1,1,0,1,1],  # 12
-    [0,0,0,0,0,0,0,0,0,0,0,1,1,0,1],  # 13
-    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0]   # 14
-]
-
-
-
-
-
-    
-
-    
 
     bording_region_dict = create_bording_region_dict(adj)
-    initial_results_dict = create_initial_results_dict(bording_region_dict)
-    result_dict = allocate_color(bording_region_dict, initial_results_dict, True)
+    results_dict = create_initial_results_dict(bording_region_dict)
+    allocate_color(bording_region_dict, results_dict, True)
     
-    
-    for region, color in result_dict.items():
+    for region, color in results_dict.items():
         print(f"{region}  Color {color}")
 
-    create_connected_node_network(bording_region_dict, result_dict)
+    create_connected_node_network(bording_region_dict, results_dict)
 
 if __name__ == "__main__":
 	main()
